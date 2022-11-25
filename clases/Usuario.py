42d5583211a6b39ef6b0651b1ec9eecc9f4f9093
Bryan Villanueva <sistemas89@red5g.co>
2022-11-25 15:44:00 -0500

    soulucion conflicto

--- clases/Usuario.py
+++ clases/Usuario.py
@@ -1,12 +1,11 @@
 import datetime
 from flask import Flask, jsonify, request, Blueprint
 from model.Usuario import Usuarios as usuariosModel
+from model.Roles import Roles as rolesModel
 import json
 from utils.db import db
+from utils.jwt_funciones import token as jwt_token
 import jwt
-
-
-key = 'lnf1ern0xsex'
 
 
 class Usuario():
@@ -21,7 +20,7 @@ class Usuario():
         try:
             if request.method == 'GET':
                 data = []
-                usuarios = usuariosModel.query.filter_by(ID_ROL=3).all()
+                usuarios = usuariosModel.query.filter_by(ID_ROL=2).all()
             if len(usuarios) == 0:
                 return jsonify({'message': 'no hay usuarios'})
             else:
@@ -83,37 +82,34 @@ class Usuario():
 
     def login(data):
         try:
+            # print("aqui dadta ------------")
+            # print(data["CORREO"])
+            test = ["ROL", "ID_USUARIO", "CEDULA",
+                    "NOMBRES", "APELLIDOS", "CORREO"]
+            dict = {}
+            cont = 0
 
-            userbd = db.session.query(usuariosModel).filter(
-                usuariosModel.CORREO == data["CORREO"], usuariosModel.CONTRASENA == data["CONTRASENA"]).first()
+            userbd = db.session.query(rolesModel.ROL, usuariosModel.ID_USUARIO, usuariosModel.CEDULA, usuariosModel.NOMBRES, usuariosModel.APELLIDOS, usuariosModel.CORREO).join(
+                rolesModel, rolesModel.ID_ROL == usuariosModel.ID_ROL).filter(usuariosModel.CORREO == data["CORREO"], usuariosModel.CONTRASENA == data["CONTRASENA"]).first()
+            # print("aqui consulta --------->")
+            # print(userbd)
+            # for i in userbd:
+            #     dict[test[cont]] = i
+            #     cont += 1
+            # cont = 0
+
+            # # print(userbd1[1].ROL)
+            # print("aquiiiiiiiiiiiiiiiii")
+            # print(data_dict)
+            # print(dict)
+            # userbd = db.session.query(usuariosModel).filter(
+            #     usuariosModel.CORREO == data["CORREO"], usuariosModel.CONTRASENA == data["CONTRASENA"]).first()
+            # print(dict)
             if userbd != None:
-
-                token = jwt.encode({'public_id': userbd.ID_USUARIO, 'exp': datetime.datetime.utcnow(
-                ) + datetime.timedelta(minutes=60)}, key, algorithm="HS256")
-                return jsonify({'message': "ok",
-                                'userId': userbd.ID_USUARIO,
-                                'rol': userbd.ID_ROL,
-                                'token': token,
-                                'name': userbd.USUARIO
-                                })
+                data_dict = {test[i]: userbd[i] for i in range(0, len(userbd))}
+                token = jwt_token(data_dict)
+                return jsonify({'message': "ok", 'token': token})
             else:
-                return jsonify({'message': "fail"})
-        except Exception as e:
-            return jsonify({'message': str(e)})
-
-    def informacionPerfil(data):
-        try:
-
-            userbd = db.session.query(usuariosModel).filter(
-                usuariosModel.ID_USUARIO == data["ID_USUARIO"]).first()
-            if userbd != None:
-                return jsonify({
-                    'NOMBRES': userbd.NOMBRES,
-                    'APELLIDOS': userbd.APELLIDOS,
-                    'CORREO': userbd.CORREO,
-                    'ID_ROL': userbd.ID_ROL
-                })
-            else:
-                return jsonify({'message': "!!!Error!!! no se ha encontrado el usuario"})
+                return jsonify({'message': "Error usuario no encontrado, Volver a intentar"})
         except Exception as e:
             return jsonify({'message': str(e)})
